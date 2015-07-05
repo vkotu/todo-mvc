b9dcc4134cee926aa3c9b41f343f131f56afe374
venkat kotu <vikkii89@gmail.com>
2015-07-04 21:30:57 -0700

    todo-mvc app

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('todos',{path:'/'});
+  this.route('todos');
 });
 
 export default Router;
---
+++ app/routes/todos.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model: function() {
+        return this.store.find('todo');
+    },
+    actions: {
+        createTodo: function(newTitle) {
+            // Create the new Todo model
+            var todo = this.store.createRecord('todo', {
+                title: newTitle,
+                isCompleted: false
+            });
+
+            // Clear the "New Todo" text field
+            this.controllerFor('todos').set('newTitle', '');
+
+            // Save the new model
+            todo.save();
+        }
+    }
+
+});
